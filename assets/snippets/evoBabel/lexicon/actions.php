ff2d2af5c4b922371f5aadb80d3dea067cb4eda2
webber12 <web-ber12@yandex.ru>
2013-12-02 14:08:57 +0200

    add lexicon module files

---
+++ assets/snippets/evoBabel/lexicon/actions.php
@@ -0,0 +1,75 @@
+<?php
+if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+
+	define('MODX_API_MODE', true);
+	
+	if(isset($_GET['manager_path'])){
+	$manager_path=$_GET['manager_path'];
+
+	
+	include_once $manager_path.'includes/config.inc.php';
+	include_once $manager_path.'includes/protect.inc.php';
+	include_once $manager_path.'includes/document.parser.class.inc.php';
+	$modx = new DocumentParser;
+	$modx->db->connect();
+	$modx->getSettings();
+
+	//работа с данными - удаление, сохранение, обновление
+	if(isset($_GET['action'])){
+		$action=$_GET['action'];
+		switch ($action){
+			case 'get':
+				$q=$modx->db->query("SELECT * FROM ".$modx->getFullTableName('lexicon')." ORDER BY name ASC");
+				$list = $modx->db->makeArray($q);
+				$a=json_encode($list);
+				echo $a;
+			break;
+			case 'save':
+				$out='';
+				foreach($_POST as $k=>$v){
+					if($k!='isNewRecord'){
+						$fields[$k]=$v;
+					}
+					$out.=$k.'='.$v.';';
+				}
+				if($_POST['isNewRecord']){
+					$modx->db->insert($fields,$modx->getFullTableName('lexicon'));
+				}
+			break;
+			case 'update':
+				$out='';
+				foreach($_POST as $k=>$v){
+					if($k=='id'){$id=$v;}
+					else{
+						$fields[$k]=$v;
+					}
+					$out.=$k.'='.$v.';';
+				}
+				if($id){
+					$modx->db->update($fields,$modx->getFullTableName('lexicon'),'id='.$id);
+				}
+			break;
+			case 'destroy':
+				$out='';
+				foreach($_POST as $k=>$v){
+					if($k=='id'){$id=$v;}
+				}
+				if($id){
+					$modx->db->delete($modx->getFullTableName('lexicon'),'id='.$id);
+				}
+			break;
+			
+			default:
+			break;
+		}
+	}	
+	
+
+
+}
+
+//exit;
+}
+
+
+?>
